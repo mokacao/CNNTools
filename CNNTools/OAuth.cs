74bc5ea5f477b289c9ce497fe49ef641f14a77d8
arturo2019 <arturo.mendoza.u@gmail.com>
2014-08-12 22:26:57 -0400

    Proyecto CNN
    Hasta el momento se incorpora un proyecto dummy

---
+++ CNNTools/OAuth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CNNTools
+{
+    public class OAuth : OAuthImpl
+    {
+        public string GetUrlCNN()
+        {
+            return "URL CNN";
+        }
+    }
+}
